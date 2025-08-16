82dd15b30f149bd96fac7c868f82b226d5df47f2
huahua8163 <huahua8163@gmail.com>
2025-08-16 14:04:45 +0800

    lesson8介面 機器人回覆

---
+++ lesson8/lesson8_5.py
@@ -0,0 +1,31 @@
+import gradio as gr
+
+with gr.Blocks() as demo:
+    a = gr.Number(label="輸入數字A", value=0)
+    b = gr.Number(label="輸入數字B", value=0)
+
+    with gr.Row():
+        add_button = gr.Button("加法")
+        subtract_button = gr.Button("減法")
+        multiply_button = gr.Button("乘法")
+        divide_button = gr.Button("除法")
+
+    C = gr.Number(label="結果", value=0)
+
+    @add_button.click(inputs=[a, b], outputs=[C])
+    def devide(a, b):
+        return a + b
+    
+    @subtract_button.click(inputs=[a, b], outputs=[C])
+    def subtract(a, b):
+        return a - b
+    
+    @multiply_button.click(inputs=[a, b], outputs=[C])
+    def multiply(a, b):
+        return a * b
+    
+    @divide_button.click(inputs=[a, b], outputs=[C])
+    def divide(a, b):
+        return a / b
+    
+    demo.launch()
---
+++ lesson8/lesson8_6.py
@@ -0,0 +1,32 @@
+import gradio as gr
+
+# 建立gradio Blocks架構
+with gr.Blocks() as demo:
+    gr.Markdown("## 公司內部機器人")
+    #建立輸入框
+    input_text = gr.Textbox(label="請輸入您的姓名", placeholder="請輸入您的姓名", submit_btn=True)
+
+    #建立gradio.Accordion
+    with gr.Accordion("點擊展開常見Q&A", open=False):
+        gr.Examples(
+            examples=[
+                ["2025國定假日?"],
+                ["這個AI機器人可以做什麼?"],
+                ["如何聯繫人資部?"]
+            ],
+            inputs=input_text
+        )
+
+ 
+     #建立輸出框
+    output_text = gr.Textbox(
+        label="機器人的回覆", 
+        placeholder="輸出結果會顯示在這裡",
+        interactive=False)
+
+    @input_text.submit(inputs=[input_text], outputs=[output_text])
+    def respond(message):
+        # 在這裡處理用戶訊息
+        return "這是機器人的回覆"
+
+    demo.launch()
