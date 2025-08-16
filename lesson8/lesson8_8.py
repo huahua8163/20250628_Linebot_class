42ddc3dfebd67826b555b45a1f715b09d20a6a2b
huahua8163 <huahua8163@gmail.com>
2025-08-16 15:24:22 +0800

    更新

--- lesson8/lesson8_8.py
+++ lesson8/lesson8_8.py
@@ -5,7 +5,7 @@ client = genai.Client()
 
 with gr.Blocks() as demo:
     gr.Markdown("## Text to Summarization(總結)")
-    style_radio = gr.Radio(choices=["口語", "學術","專業","簡潔","俏皮"], label="摘要風格")
+    style_radio = gr.Radio(choices=["口語", "小學","專業","簡潔","俏皮"], label="摘要風格")
     input_text = gr.Textbox(
         label="請輸入要總結的文本", 
         placeholder="請輸入要總結的文本", 
@@ -18,8 +18,8 @@ with gr.Blocks() as demo:
     def summarize(text, style):
         if style == "口語":
             style = "請轉換口語風格\n"
-        elif style == "學術":
-            style = "請使用學術風格\n"
+        elif style == "小學":
+            style = "請使用小學生看得懂的風格\n"
         elif style == "專業": 
             style = "請使用專業風格\n"
         elif style == "簡潔":
